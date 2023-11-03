9ed1fca5f76eeb7b73bc97f50b0e5d898b9de536
be-julia <149503086+be-julia@users.noreply.github.com>
2023-11-03 08:42:19 +0100

    Install Svelte
    - add shorter data-list

---
+++ vite-map/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+  import cities from './data/cities-test.json';
+</script>
+
+<main>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+  <ul>
+    {#each cities as city,i}
+      <li>
+          {i + 1} {city.name}
+      </li>
+    {/each}
+  </ul>
+
+</main>
+
+<style>
+.dot {
+  background-color: #4cc35b;
+}
+</style>
